8d61744ad8fb6ab5b4145c23e7afd3af71e3317e
Bahanur Enis <bahanurenis@gmail.com>
2024-10-12 18:03:43 +0200

    customize configurations to benis

--- nix/git.nix
+++ nix/git.nix
@@ -13,11 +13,10 @@ with lib;
 
   programs.git = {
     enable = true;
-    userName = "Talha Can Havadar";
-    userEmail = "havadartalha@gmail.com";
+    userName = "Bahanur Enis";
+    userEmail = "bahanurenis@gmail.com";
     extraConfig = {
       commit.gpgSign = "true";
-      includeIf."gitdir:~/workspace/".path = "~/workspace/.gitconfig";
     };
   };
 }
